4bcf68e74c06615b0482a0f26f635e4ca48b99fe
Kobayashi Daisuke <daisuke.koba@gmail.com>
2023-03-20 23:29:33 +0900

    Implement.

--- BleScanner/Program.cs
+++ BleScanner/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using Windows.Devices.Bluetooth.Advertisement;
+
+var watcher = new BluetoothLEAdvertisementWatcher
+{
+    ScanningMode = BluetoothLEScanningMode.Active
+};
+watcher.Received += (sender, args) =>
+{
+    var line = new StringBuilder();
+    line.Append(args.Timestamp.ToString("HH:mm:ss.ffff"));
+    line.Append("  ");
+    line.Append(args.BluetoothAddress.ToString("x12"));
+    line.Append("  ");
+    line.Append(args.Advertisement.LocalName);
+    Console.WriteLine(line.ToString());
+};
+watcher.Start();
+await Task.Delay(30000);
+watcher.Stop();
